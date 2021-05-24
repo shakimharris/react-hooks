8e98617db3b566899bea162514aad157f3a17cbb
Shakim Harris <amenraprod@gmail.com>
2021-05-23 22:24:59 -0400

    changed class component to functional component

--- src/components/Two.js
+++ src/components/Two.js
@@ -1,22 +1,14 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Two extends Component {
-  state = {
-    activeUser: "Chris"
-  };
-  changeUser = () => {
-    this.setState({ activeUser: "Bolingo!" });
-  };
-
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 2</h2>
-        <p>Active User is: {this.state.activeUser}</p>
-        <button onClick={this.changeUser}>Change Me!</button>
-      </div>
-    );
-  }
+function Two() {
+  const [activeUser, setactiveUser] = useState("Osiris");
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 2</h2>
+      <p>Active User is: {activeUser}</p>
+      <button onClick={() => setactiveUser(activeUser)}>Change Me!</button>
+    </div>
+  );
 }
 
 export default Two;
